b05af9b91d0d61f0a636a9debefd4341fe28bf56
vexonelite <elite@realtime.tw>
2017-09-08 16:12:05 +0800

    chap 8 revision #2

--- book_chap8_arrays.playground/Contents.swift
+++ book_chap8_arrays.playground/Contents.swift
@@ -102,7 +102,81 @@ print(upcomingPlayers2)
 
 
 
+// [ - Checking for an element - ]
+// You can check if there’s at least one occurrence of a specific element in an array by using contains(_:), which returns true if it finds the element in the array, and false if it doesn’t.
+func isEliminated(player: String) -> Bool {
+    return !players.contains(player)
+}
+print(isEliminated(player: "Bob"))
+
+print(players[1...3].contains("Bob"))
 
 
 
+// [3. Modifying Arrays]
+// You can make all kinds of changes to mutable arrays: adding and removing elements, updating existing values, and moving elements around into a different order.
+
+
+// [ - Appending elements - ]
+players.append("Eli")
+players += ["Gina"]
+
+
+// [ - Inserting elements - ]
+players.insert("Frank", at: 5)
+if let position = players.index(of: "Frank") {
+    players.insert("Fuck", at: position + 1)
+}
+
+
+// [ - Removing elements - ]
+var removedPlayer = players.removeLast()
+print("\(removedPlayer) was removed")
+
+removedPlayer = players.remove(at: 2)
+print("\(removedPlayer) was removed")
+
+// But how would you get the index of an element if you didn’t already know it?There’s a method for that! index(of:) returns the "first index" of the element, because the array might contain multiple copies of the same value. If the method doesn’t find the element, it returns nil.
+
+if let position = players.index(of: "Fuck") {
+    removedPlayer = players.remove(at: position)
+    if removedPlayer != nil {
+        print("\(removedPlayer) was removed")
+    }
+}
+
+
+// [ - Updating elements - ]
+print(players)
+// > ["Alice", "Bob", "Dan", "Eli", "Frank"]
+// you should use the subscript syntax to update the name:
+players[4] = "Franklin"
+print(players)
+// > ["Alice", "Bob", "Dan", "Eli", "Franklin"]
+
+// use subscripting with ranges to update multiple values in a single line of code:
+players[0...1] = ["Donna", "Craig", "Brian", "Anna"]
+print(players)
+// > ["Donna", "Craig", "Brian", "Anna", "Dan", "Eli", "Franklin"]
+// This code replaces the first two players, Alice and Bob, with the four players in the new players array. 
+// ** As you can see, the size of the range doesn’t have to be equal to the size of the array that holds the values you’re adding. **
+
+
+// [ - Moving elements - ]
+
+// Mehod #1 - use remove() and insert()
+//let playerAnna = players.remove(at: 3)
+//players.insert(playerAnna, at: 0)
+//print(players)
+// > ["Anna", "Donna", "Craig", "Brian", "Dan", "Eli", "Franklin"]
+
+// Method #2 - use sort()
+//players.sort()
+//print(players)
+// > ["Anna", "Brian", "Craig", "Dan", "Donna", "Eli", "Franklin"]
+
+// If you’d like to leave the original array untouched and return a sorted copy instead, use sorted() instead of sort().
+let sortedPlayers = players.sorted()
+print("players is \(players)")
+print("sortedPlayers is \(sortedPlayers)")
 
