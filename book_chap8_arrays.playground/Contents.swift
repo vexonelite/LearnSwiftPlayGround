aa971fb91cbf6632d312b8ceb9867df12add52a9
vexonelite <elite@realtime.tw>
2017-09-08 15:04:35 +0800

    chap 8 revision #1

--- book_chap8_arrays.playground/Contents.swift
+++ book_chap8_arrays.playground/Contents.swift
@@ -11,6 +11,9 @@ import UIKit
 // If you need to add, remove or update values in the array, then you should create a mutable array by declaring it as a variable.
 
 
+
+// [1. Creating arrays]
+
 // array literal
 let evenNumbers = [2, 4, 6, 8]
 let nameString = ["John", "Mary", "Tom", "Paul", "Anne"]
@@ -23,27 +26,83 @@ let allZeros = [Int](repeating: 0, count: 5)
 // type inference
 let allZerosInferred = Array(repeating: 0, count: 5)
 
+
+//Finally, remember that it’s good practice to declare arrays that aren’t going to change as constants. For example, consider this array:
+let vowels = ["A", "E", "I", "O", "U"]
+
+
+
 // Immutable
 let playersC: [String] = ["Alice", "Bob", "Cindy", "Dan"]
 // Muutable
 var players: [String] = ["Alice", "Bob", "Cindy", "Dan"]
 print(players.isEmpty)
 
+
+
+// [2. Accessing elements]
+
+// [ - Using Array's properties and methods - ]
+
+// isEmpty property
+print(players.isEmpty)
+
+
+// count property
 if players.count < 2 {
     print("We need at least two players!")
 } else {
     print("Let's start!")
 }
 
-// warning here:
-print(players.first)
-print(players.last)
-print(players.min())
-print(players.max())
 
+// property first/ last will return an optional
+// also, method min() and max() will return optional
+
+// warning here:
+//print(players.first)
+//print(players.last)
+//print(players.min())
+//print(players.max())
+var currentPlayer = players.first
+//print(currentPlayer)
+currentPlayer = players.min()
+//print(currentPlayer)
+
+
+// using optional binding to do
 if let firstPlayer = players.first {
     print(firstPlayer)
 }
+
+if let minPlayer = players.min() {
+    print(minPlayer)
+}
+
+
+// [ - Using subscripting - ]
+
+var firstPlayer = players[0]
+print("First player is \(firstPlayer)")
+
+
+//When you use subscripts, you don’t have to worry about optionals, since trying to access a non-existing index doesn’t return nil; it simply causes a runtime error. (it will make app crash)
+
+//var wrongPlayer = players[4]
+// > fatal error: Index out of range
+
+
+// [ - Using ranges - ]
+let upcomingPlayers = players[1...2]
+print("upcoming players are \(upcomingPlayers)")
+
+
+let upcomingPlayers2 = players[1 ..< players.count]
+print(upcomingPlayers2)
+
+
+
+
 
 
 
