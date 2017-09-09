fd81fa88ec605aa76516c3549b02188117d2eef8
vexonelite <elite@realtime.tw>
2017-09-09 15:15:40 +0800

    1. add book_chap10_collection_iteration_with_closures.playground; 2. complete book_chap9_dictionaries.playground

---
+++ book_chap10_collection_iteration_with_closures.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Chapter 10: Collection Iteration with Closures
+
+// The previous section taught you about "functions". But Swift has another object you can use to "break up code into reusable chunks: a closure". They become particularly useful when dealing with collections.
+
+// A closure is simply "a function with no name"; you can assign it to a variable and pass it around like any other value. This chapter shows you how convenient and useful closures can be.
